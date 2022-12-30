73224ef2de2235528cbf5a2636c61c78468bfd9c
pulak-gautam <pulak.gautam@gmail.com>
2022-12-30 13:54:42 +0530

    files

---
+++ client.py
@@ -0,0 +1,26 @@
+import socket 
+import time
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' '*(HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(HEADER).decode(FORMAT))
+
+send("Hello World")
+# time.sleep(5)
+send(DISCONNECT_MESSAGE)
